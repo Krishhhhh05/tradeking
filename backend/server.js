3dc3893c1b120a6fd74cf0846efe87671d01c30e
Krishhhhh05 <krishaborana@gmail.com>
2025-07-27 22:08:29 +0530

    feat: update CORS configuration to allow requests from Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://tradeking-tan.vercel.app"]
   })
 );
 app.use(express.json());
